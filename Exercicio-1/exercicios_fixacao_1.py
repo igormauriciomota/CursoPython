4fa6645d50deba61fa23de683a74f186fd3336ac
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-12-09 18:41:07 -0300

    Exercicio de fixação

--- Exercicio-1/exercicios_fixacao_1.py
+++ Exercicio-1/exercicios_fixacao_1.py
@@ -54,33 +54,69 @@ print(f'O Preço a pagar e de R$ {preco_final}')
 
 6) Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
 
+distancia = float(input('Digite a distancia a percorrer em KM: '))
+velocidade_media = float(input('Digite a velocidade media em km/h: '))
+
+tempo_viagem = distancia / velocidade_media
+
+print(f'O tempo de viagem será de {tempo_viagem} horas')
+
+
 7) Converta uma temperatura digitada em Celsius para Fahrenheit. F = 9*C/5 + 32
 
+celsius = float(input('Digite a temperatura em Celsius: '))
+
+fahrenheit = (9 * celsius /5) + 32
+
+print(f'A temperatura em fahrenheit e {fahrenheit}')
+
+
 8) Faça agora o contrário, de Fahrenheit para Celsius.
+
+fahrenheit = float(input('Digite a temperatura em Fahrenheit: '))
+
+celsius = (fahrenheit - 32) * 5 / 9
+
+print(f'A temperatura em Celsius e {celsius}')
+
 
 9) Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim 
 como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro custa 
 R$ 60,00 por dia e R$ 0,15 por km rodado.
 
+km_percorrido = float(input('Digite a quntidade de km percorrido: '))
+dias_aluguel = int(input('Digite a quantidades de dias pelo qual o carro foi alugado: '))
+preco_diario = float(input('Digite o preço da diaria: '))
+preco_km = float(input('Digite o peço do km por litro rodado: '))
+
+preco_a_pagar = (dias_aluguel * preco_diario) + (km_percorrido * preco_km)
+
+print(f'O Preço a pagar é de R${preco_a_pagar}')
+
 10) Escreva um programa para calcular a redução do tempo de vida de um fumante. 
 Pergunte a quantidade de cigarros fumados por dia e quantos anos ele já fumou. Considere que um fumante perde 
 10 minutos de vida a cada cigarro, calcule quantos dias de vida um fumante perderá. Exiba o total de dias.
 
-11) Sabendo que str( ) converte valores numéricos para string, calcule quantos dígitos há em 2 elevado a um milhão.
+cigarro_por_dia = int(input('Digite a quantidade de cigarros fumados por dia: '))
+anos_fumando = int(input('Digite a quantidades de anos que voce já fumou: '))
+
+total_cigarros = cigarro_por_dia * (anos_fumando * 365)
+
+minutos_perdidos = total_cigarros * 10
+dias_perdidos = minutos_perdidos / 1440
+
+print(f'O total de dias de vida perdidos é de {dias_perdidos}')
+
+
+11) Sabendo que str( ) converte valores numéricos para string, calcule quantos dígitos há em 2 elevado a 1000.
+
+resultado = 2 ** 1000
+
+quantidade_digitos = len(str(resultado))
+
+print(f'A quantidade de digitos em 2 elevado a mil é {quantidade_digitos}')
 
 '''
-# 1
-# 2
-# 3
-# 4
-# 5
 
-preco_mercado = float(input('Digite o preco da mercadoria: '))
-percentual_desconto = float(input('Digite o percentual de desconto: '))
 
-valor_desconto = preco_mercado * (percentual_desconto / 100)
-preco_final = preco_mercado - valor_desconto
-
-print(f'O valor do desconto e: R$ {valor_desconto}')
-print(f'O Preço a pagar e de R$ {preco_final}')
 
--- Exercicio_listas/exercicio_lista_pt2.py
+++ Exercicio_listas/exercicio_lista_pt2.py
@@ -9,3 +9,24 @@ Ao sair indicar que o caixa esta fechado. O usuario deve controlar o sistema da 
 as vendas e as compras de estoque, sem esquecer de alterar os valores da lista de quantidade.  
 
 """
+jogos = ['Gow', 'Minecraft', 'GTA', 'Sonic', 'FIFA'] # Jogo disponiveis para venda
+precosVenda = [210.00, 2.99, 150.00, 1.80, 125.00] # Preço de venda
+precosCompraEstoque = [105.00, 1.50, 75.00, 0.90, 62.50] # Preços para o dono da loja comprar da fabrica
+quantidade = [3, 0, 2, 5, 1] # Quantidade de jogos disponiveis
+vendas = [] # Vendas registradas
+compraDeEstoque = []
+
+op = 0
+
+while op != 4:
+    print('1 - registrar vendas\n2 - Compra de estoque\n3 - Resumo da loja\n4 - Sair')
+    op = int(input('Opção: '))
+    
+    if op == 1:
+        jogoVendido = input('Jogo Vendido: ')
+        quantidadeVendida = int(input('Quantidade vendida: '))
+        if quantidadeVendida <= quantidade[jogos.index(jogoVendido)]: # Testa se a quantidade que o
+            print(f'\n{jogoVendido} vendido!')
+            quantidade[jogos.index(jogoVendido)] -= quantidadeVendida # Remove do estoque a quantidade vendida
+            
+            vendas.append(precosVenda)
