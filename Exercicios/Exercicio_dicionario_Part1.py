d108981f68e67084b5524cc457d4ad9b06648de6
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-12-13 23:00:21 -0300

    Exercicios dicionario part1

--- Exercicios/Exercicio_dicionario_Part1.py
+++ Exercicios/Exercicio_dicionario_Part1.py
@@ -19,3 +19,26 @@ herói imprima a arma usada. Caso resulte em empate informe na saída.
 Observação: Pode definir qualquer herói,vilao e arma que quiser.
 
 """
+herois = {'Homem de Ferro':80, 'Capitam Marvel':98, 'Thor':93, 'Goku':100}
+armas = {'Mionir':90, 'Hukebaster': 95, 'Escudo':85, 'Raio de zeus': 98, 'Kaioken':98}
+viloes = {'Dike vigarista':85, 'Venon':145, 'Thanos':185, 'Friza':165, 'Ultron':182}
+
+herois_escolhido = input('Digite o heroi escolhido: ')
+arma_sescolhida = input('Digite a arma escolhida: ')
+vilao_escolhido = input('Digite o vilao escolhido: ')
+
+nivel_poder_heroi = herois[herois_escolhido] + armas[arma_sescolhida]
+
+if nivel_poder_heroi > viloes[vilao_escolhido]:
+    print(f'O vencedor é {herois_escolhido} com {arma_sescolhida}')
+elif nivel_poder_heroi == viloes[vilao_escolhido]:
+    print('Deu empate!')
+else:
+    print(f'O Vilão {vilao_escolhido}! Planeta em perigo!')
+
+# Resposta?
+
+# Digite o heroi escolhido: Goku
+# Digite a arma escolhida: Kaioken
+# Digite o vilao escolhido: Thanos
+# O vencedor é Goku com Kaioken
