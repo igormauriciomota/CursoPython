39ca0a1fe3bd65859a0b3582f508fea2cd491b27
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-12-20 18:43:37 -0300

    Exercício lambda

---
+++ Exercicios/Exercicio_lambda.py
@@ -0,0 +1,7 @@
+'''
+1 - Criar duas listas de mesmo tamanho preenchidas com numeros inteiros e retornar outra lista com
+a soma das duas listas sendo uma delas invertida (indic 0 com indice 9 para uma lista de tamanho 10,
+por exemplo), utilizando lambda e comprehension para iterar em ambas
+
+'''
+
--- Python/Funcoes_lambdas.py
+++ Python/Funcoes_lambdas.py
@@ -21,22 +21,51 @@ print(pot(3, 2))
 
 # lambda = Expreços lambda aceita qualquer quantidade de parametros de aentrada
 
-# Exemplo 1 de expreção lambda
+(I) Exemplo de expreção lambda
+
 post = lambda x, y: x ** y
 print(post(3, 2))
 
 post = lambda x, y: print(x ** y)
 post(3, 2)
 
-# Exemplo 2 de expreção lambda
+(II) Exemplo de expreção lambda
+
 cadastro = lambda nome, idade: f'Nome: {nome.title()}\nIdade: {idade}'
 print(cadastro('Pedro', 24))
 
+--- ou ----
+
+cadastro = lambda nome, idade: print(f'Nome: {nome.title()}\nIdade: {idade}')
+cadastro('Pedro', 24)
+
+Código problemático:
+
+foo = lambda x: x**2 + 2 * x + 1  # [unnecessary-lambda-assignment]
+
+Código correto:
+
+def foo(x):
+    return x**2 + 2 * x + 1
+    
+(III) Exemplo 
+
+a)
+segGrau = lambda a, b, c, x: a * x ** 2 + b * x + c
+print(segGrau(1, 5, 8, 3))
+
+b)
+def quadratica(a, b, c):
+    return lambda x: a * x ** 2 + b * x + c
+segGrau = quadratica(1, 5, 8)
+print(segGrau(3))
 
 '''
 
 
-# Exemplo 2 de expreção lambda
-cadastro = lambda nome, idade: print(f'Nome: {nome.title()}\nIdade: {idade}')
-cadastro('Pedro', 24)
+# Outra forma de fazer o exemplo 3
+def quadratica(a, b, c):
+    return lambda x: a * x ** 2 + b * x + c
+segGrau = quadratica(1, 5, 8)
+print(segGrau(3))
 
