0cfe33ba137362dede5b1b331633204282f3b7db
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-12-22 16:47:50 -0300

    exercício fixação

---
+++ Exercicio-1/Exerci_oper_logicos.py
@@ -0,0 +1,34 @@
+# O peradores Logicos
+'''
+Exercicio 1 - Operador Lógico AND (and)
+
+a = 5
+b = 10 
+
+resultado = (a < 10 and b > 0)
+print(f'Ambas as condiçoes são verdadeiras? {resultado}') # True/verdade
+
+Exercicio 2 - Operador Lógico OR (or)
+
+a = 5
+b = 10 
+
+resultado = (a > 10 and b > 0)
+print(f'Pelo menos uma das condiçoes são verdadeiras? {resultado}') # False
+
+Exercicio 3 - Operador Lógico OR (or)
+
+a = 5
+b = 10 
+
+resultado = not (a > b)
+print(f'A condição invertida é verdadeira? {resultado}') # True
+
+
+'''
+a = 5
+b = 10 
+
+resultado = not (a > b)
+print(f'A condição invertida é verdadeira? {resultado}')
+
