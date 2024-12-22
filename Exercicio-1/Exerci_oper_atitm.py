7b2694e06003e049f6030ab0fd05c940ed522d5f
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-12-22 16:36:17 -0300

    Exercício sobre operadores

---
+++ Exercicio-1/Exerci_oper_atitm.py
@@ -0,0 +1,37 @@
+# Operadores Aritimeticos
+
+a = 5
+b = 3
+c = 10
+d = 4
+e = 7
+f = 6
+g = 2
+
+# Adição
+soma = a + b
+print(soma)
+
+# Subtração
+menos = c - d
+print(menos)
+
+# mutiplicação
+multi = e * f
+print(multi)
+
+# divição
+divis = c / d
+print(divis)
+
+# modulo
+mod = c % f
+print(mod)
+
+# exponencial
+expo = f ** g
+print(expo)
+
+# Divisão inteira
+div_int = f // g
+print(div_int)
---
+++ Exercicio-1/Exerci_oper_comparacao.py
@@ -0,0 +1,82 @@
+# Exercicios Operadores de Comparação
+
+'''
+Exercicio 1 Operador de Igualdade (==)
+
+a = 5
+b = 5
+c = 6
+
+resultado1 = (a == b) # True
+resultado2 = (c == b) # False
+print(resultado1)
+print(resultado2)
+
+Exercicio 2 Operador de Diferença (!=)
+
+a = 5
+b = 5
+c = 6
+
+resultado1 = (a != b)
+resultado2 = (c != b)
+print(resultado1) # False
+print(resultado2) # True
+
+
+Exercicio 3 Operador Maior que (>)
+
+a = 5
+b = 5
+c = 6
+
+resultado1 = (a > b)
+resultado2 = (c > b)
+print(resultado1) # False
+print(resultado2) # True
+
+Exercicio 4 Operador Menor que (<)
+
+a = 5
+b = 5
+c = 4
+
+resultado1 = (a < b) 
+resultado2 = (c < b)
+print(resultado1) # False
+print(resultado2) # True (4 menor 5)
+
+Exercicio 5 Operador Maior ou Igual (>=)
+
+a = 5
+b = 5
+c = 6
+
+resultado1 = (a <= b) # 5 Maior ou igual 5
+resultado2 = (c <= b) # 6 Maior ou igual 5
+print(resultado1) # True 
+print(resultado2) # True
+
+Exercicio 6 Operador Menor ou Igual (<=)
+
+a = 5
+b = 5
+c = 4
+
+resultado1 = (a <= b) # 5 menor ou igual 5
+resultado2 = (c <= b) # 4 menor ou igual 5
+print(resultado1) # True 
+print(resultado2) # True
+
+'''
+
+a = 5
+b = 5
+c = 6
+
+resultado1 = (a >= b)
+resultado2 = (c >= b)
+print(resultado1) # False
+print(resultado2) # True
+
+
