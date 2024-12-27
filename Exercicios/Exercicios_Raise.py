6a7657e059ae17bd3b81eba591a1f00cb8561ab2
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-12-27 10:30:24 -0300

    Raise tratamento de erro

---
+++ Exercicios/Exercicios_Raise.py
@@ -0,0 +1,29 @@
+'''
+1) A partir do seguinte código aplique raise para apresentar possíveis erros do
+seguinte programa que realiza a escolha de uma das opções de lazer para uma
+pessoa e dependendo da escolha, chama uma função e realiza testes
+específicos.
+
+def viagem_exterior(nome,idade,valor):
+    print(f'{nome},{idade} anos, tem {valor} para gastar em viagens')
+    
+def fazer_compras(nome,idade,valor):
+    if valor < 200:
+        print('Seu orcamento esta curto, cuidado!')
+    print(f'{nome},{idade} anos, tem {valor} para gastar em compras')
+
+def cassino(nome,idade,valor):
+    if idade < 18:
+        print('Você não pode entrar')
+    print(f'{nome},{idade} anos, tem {valor} para gastar em cassinos')
+    
+atividade = input('Digite a opcao que deseja: viajar_exterior,
+fazer_compras ou cassino \n Opcao:')
+if atividade == 'viajar_exterior':
+    viagem_exterior(nome='Bruno',idade=26,valor=300.0)
+elif atividade == 'fazer_compras':
+    fazer_compras(nome='Isabela',idade=40,valor=150.0)
+elif atividade == 'cassino':
+    cassino(nome='Eduardo',idade=15,valor=100.0)
+    
+'''
--- Python/Raise.py
+++ Python/Raise.py
@@ -1,4 +1,61 @@
 '''
 Raise
 
+Tratar erros
+
+- A palavra Eaise significa levantar, em programação e utilizada para apresentar/levantar erros no 
+codigo que náo mesmos identificamos 
+
+- A palavra reise é reservada assim com return, def, break,.... 
+
+- Caso voce utilize o 'raise' dentro de uma função, assim que o programa o reconhecer ira acusar o erro e sair da função.  
+
+? como declarar?
+
+raise tipo_do_erro('Mensagem que voce deseja que apareça')
+
+Ex:
+
+A) Cadastramento em um site:
+
+(1º) Fase
+
+# função de cadastro
+def cadastrar(login, senha):
+    print(f'Login: {login} e Senha: {senha}')
+    
+#Chamar a função
+cadastrar(123, 123)
+
+(2°) Fase
+
+# função de cadastro
+def cadastrar(login, senha):
+    if type(login) is not str: # is comparação de tipos / == comparação de valores
+        raise TypeError('Login deve ser string')
+    if type(senha) is not int:
+        raise TypeError('Senha deve ser Inteiro')
+    print(f'Login: {login} e Senha: {senha}')
+    
+#Chamar a função
+cadastrar('123', 123)
+
+#Chamar a função
+cadastrar((input('Digite seu login: ')),int(input('Digite sua senha: ')))
+
+B) numero do divisor
+
+def divisao(num1,num2):
+    if num2 == 0:
+        raise ZeroDivisionError('Numero 2 não pode ser zero, pois e denominador')
+    divisao = num1/num2
+    print(f'{divisao}')
+    
+divisao(int((input('Digite um numero: '))),int(input('digite outro numero: ')))
+
+
 '''
+
+
+
+
