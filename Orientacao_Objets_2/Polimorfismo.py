d460a1f417f2d76e7b5ec4f16246a101ab670d41
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2025-01-15 19:55:17 -0300

    polimorfismo

--- Orientacao_Objets_2/Polimorfismo.py
+++ Orientacao_Objets_2/Polimorfismo.py
@@ -44,6 +44,47 @@ carne.apresentar()
 
 (II) Correto tem de ter 
 
+class Comida:
+    
+    def __init__(self, alimento):
+        self.__alimento = alimento
+        
+    @property
+    def alimento(self):
+        return self.__alimento
+    
+    # Tem de sobrescrever o Metodo apresentar tambem nas classes filhas
+    def apresentar(self):
+        raise NotImplementedError('Esse método só funciona se a sub classe implementa-lo sobrescreve-lo')
+    
+    
+class Fruta(Comida):
+    
+    def __init__(self, alimento):
+        super().__init__(alimento)
+        
+    # o Metodo apresentar tem d estar em fruta
+    def apresentar(self):
+        print(f'Sou uma fruta, voce gosta de {self.alimento}')
+        
+class Carne(Comida):
+    
+    def __init__(self, alimento):
+        super().__init__(alimento)
+    
+    # o Metodo apresentar tem de estar em carne
+    def apresentar(self):
+        print(f'Sou uma Carne, voce gosta de {self.alimento}')
+
+
+        
+# Objeto
+fruta = Fruta('Laranja')
+fruta.apresentar() 
+
+carne = Carne('Frango')
+carne.apresentar()
+
 '''
 
 class Comida:
