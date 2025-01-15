76c3e8720ac3a2a894d40d96c6b0594e6235aef7
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2025-01-15 19:53:03 -0300

    Polimorfismo em Python e exercício de fixação

---
+++ Orientacao_Objets_2/Exercicio_Polimorfismo.py
@@ -0,0 +1,11 @@
+'''
+Exercícios:
+
+1 - Crie uma superclasse chamada 'FormaGeometrica', que possui um método
+'calcula_area' e recebe o nome de uma figura geométrica em seu método
+construtor. Após isso, crie duas subclasses ('Areaquadrado' e 'Areacirculo') que
+herdam de 'FormaGeométrica' e calculam a área de sua respectiva forma. O
+método nas Classes Filhas deve ter o nome 'calcula_area', igual em sua Classe
+Mãe.
+
+'''
---
+++ Orientacao_Objets_2/Polimorfismo.py
@@ -0,0 +1,86 @@
+'''
+Polimorfismo:
+
+- Significa muitaa formas (poli - muitas; morfis - formas)
+
+- Por exemplo, um overriding é um representação de polimorfismo
+
+(I) Exemplo com error:
+
+class Comida:
+    
+    def __init__(self, alimento):
+        self.__alimento = alimento
+        
+    @property
+    def alimento(self):
+        return self.__alimento
+        
+    def apresentar(self):
+        raise NotImplementedError('Esse método só funciona se a sub classe implementa-lo sobrescreve-lo')
+    
+    
+class Fruta(Comida):
+    
+    def __init__(self, alimento):
+        super().__init__(alimento)
+        
+    def apresentar(self):
+        print(f'Sou uma fruta, voce gosta de {self.alimento}')
+        
+class Carne(Comida):
+    
+    def __init__(self, alimento):
+        super().__init__(alimento)
+
+
+        
+# Objeto
+fruta = Fruta('Laranja')
+fruta.apresentar() 
+
+carne = Carne('Frango')
+carne.apresentar()
+
+(II) Correto tem de ter 
+
+'''
+
+class Comida:
+    
+    def __init__(self, alimento):
+        self.__alimento = alimento
+        
+    @property
+    def alimento(self):
+        return self.__alimento
+        
+    def apresentar(self):
+        raise NotImplementedError('Esse método só funciona se a sub classe implementa-lo sobrescreve-lo')
+    
+    
+class Fruta(Comida):
+    
+    def __init__(self, alimento):
+        super().__init__(alimento)
+        
+    def apresentar(self):
+        print(f'Sou uma fruta, voce gosta de {self.alimento}')
+        
+class Carne(Comida):
+    
+    def __init__(self, alimento):
+        super().__init__(alimento)
+        
+    def apresentar(self):
+        print(f'Sou uma Carne, voce gosta de {self.alimento}')
+
+
+        
+# Objeto
+fruta = Fruta('Laranja')
+fruta.apresentar() 
+
+carne = Carne('Frango')
+carne.apresentar()
+
