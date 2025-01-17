250d9d3c53f945c6d3c2419d83ad82346881b4cb
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2025-01-17 17:27:24 -0300

    Exercício fixação Pickle JSON

---
+++ Arquivos_CSV_Pickle_JSON/Exercicio_Pickle_Json.py
@@ -0,0 +1,13 @@
+'''
+1) Deseja-se criar um programa que deixe a formula secreta da coca cola
+criptografada, para isso crie uma classe FormulaCocaCola com atributos
+privados: ingrediente, temperatura (Celsius), açúcar (gramas) e o nome do
+proprietário. Crie uma senha de acesso escolhida pelo usuário, instancie o
+objeto e passe o mesmo para um arquivo PICKLE. Após isso, peça a senha
+para acessar os atributos, caso esteja correta, leia o arquivo e imprima-os, se
+não imprima um aviso de acesso negado.
+
+with open('C:\\Users\\Igor\\Envs\\CursoPython\\CursoPython\\Arquivos_CSV_Pickle_JSON\\filmes.json')
+
+'''
+
--- Arquivos_CSV_Pickle_JSON/Manipulando_Pickle_e_JOSON.py
+++ Arquivos_CSV_Pickle_JSON/Manipulando_Pickle_e_JOSON.py
@@ -63,9 +63,90 @@ with open('C:\\Users\\Igor\\Envs\\CursoPython\\CursoPython\\Arquivos_CSV_Pickle_
 
 ---------------------------------JSON------------------------------------
 
+JSON -> JavaScript Object Notation
+
+- Utilizado em API´s -> Interface de Programação de aplicativos
+
+site com exemplos de JSON: http://jsonapi.org/ 
+
+Ex. 1: dumps() -> Faz a formatação para o formato JSON ("Aspas duplas")
+
+import json
+
+nome = {'Ana':'30 anos'}
+teste_json = json.dumps(nome)
+print(teste_json)
+print(type(teste_json))
+
+{"Ana": "30 anos"}
+<class 'str'>
+
+------------JUNTOS---JSON---PICKLE----------
+
+Em Python, podemos trabalhar com json e pickle juntos
+
+No terminal instale: pip install jsonpickle
+
+import jsonpickle
+
+
+class Filme:
+    
+    def __init__(self, nome, personagem):
+        self.__nome = nome
+        self.__personagem = personagem
+        
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @property
+    def personagem(self):
+        return self.__personagem    
+
+filme1 = Filme('Senhor dos Aneis', 'Frodo')
+print(jsonpickle.encode(filme1)) # encode() é utilizado para criar um dicionario no formato JSON
+# O primeiro elemento apresenta qual classe pertence esse objeto
+# {"py/object": "__main__.Filme", "_Filme__nome": "Senhor dos Aneis", "_Filme__personagem": "Frodo"}
+
+# Escrevendo arquivos 
+with open('C:\\Users\\Igor\\Envs\\CursoPython\\CursoPython\\Arquivos_CSV_Pickle_JSON\\filmes.json','w') as arq:
+    arq.write(jsonpickle.encode(filme1))
+
+-----LENDO---JSON---PICKLE----------
+
+import jsonpickle
+
+class Filme:
+    
+    def __init__(self, nome, personagem):
+        self.__nome = nome
+        self.__personagem = personagem
+        
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @property
+    def personagem(self):
+        return self.__personagem    
+
+filme1 = Filme('Senhor dos Aneis', 'Frodo')
+
+with open('C:\\Users\\Igor\\Envs\\CursoPython\\CursoPython\\Arquivos_CSV_Pickle_JSON\\filmes.json') as arq:
+    leitura = jsonpickle.decode(arq.read())
+    print(leitura)
+    print(leitura.nome)
+    print(leitura.personagem)
+
+<__main__.Filme object at 0x00000196D5D4E990>
+Senhor dos Aneis
+Frodo
 
 
 '''
 
 
-
+        
+    
+    
