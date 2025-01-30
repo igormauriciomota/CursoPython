cfc9d46eaa1f495001e53c01955b876dade02f18
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2025-01-30 12:56:03 -0300

    curiosidades comando de voz

---
+++ Curiosidades/voz.py
@@ -0,0 +1,14 @@
+"""
+pip install pyttsx3 -> é uma biblioteca de conversão de texto para fala em Python. Diferentemente de bibliotecas 
+alternativas, ela funciona offline e é compatível com Python 2 e 3.
+
+pip install pipwin -> é um pequeno gerenciador de pacotes Python. Ele instala automaticamente pip e virtualenv no 
+Windows e sua GUI permite que você: alterne de um interpretador python (ou seja, versão) para outro (incluindo py e pypy )
+
+pip install pyaudio -> 
+
+"""
+
+#----------------------------Desligar e Reiniciar o PC por voz--------------------------------
+
+
